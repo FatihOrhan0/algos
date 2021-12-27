e960ebca91e84574af99c949dbff37b8e787eda8
Fatih ORHAN <orhanfatih2002@gmail.com>
2021-12-27 17:52:13 -0500

    added the symmetric difference calculator file

---
+++ symmetricDifference.js
@@ -0,0 +1,31 @@
+function sym(args) {
+
+    if (arguments.length == 0) {
+        return [];
+    } else if (arguments.length == 1) {
+        return arguments[0];
+    }
+    let answer = [];
+    var processed = 0;
+    while (processed < arguments.length - 1) {
+        for (var i = 0; i < arguments[processed].length; i++) {
+            for (var j = 0; j < arguments[processed + 1].length; j++) {
+                if (arguments[processed][i] == arguments[processed + 1][j]) {
+                    arguments[processed].splice(i, 1);
+                    arguments[processed + 1].splice(j, 1);
+                    i--;
+                    continue;
+                }
+            }
+        }
+        processed++;
+    }
+    for (var i = 0; i < arguments.length; i++) {
+        for (var j = 0; j < arguments[i].length; j++)
+            answer.push(arguments[i][j]);
+    }
+    console.log(answer);
+    return answer;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4], [2, 6, 8]);
