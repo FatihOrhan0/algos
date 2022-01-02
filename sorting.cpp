135de8e048cb1d97da0506f6ac83429f69467d7c
Fatih ORHAN <orhanfatih2002@gmail.com>
2022-01-02 10:40:33 -0500

    corrected the quicksort and added bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -9,23 +9,28 @@ void print(std::vector<T> & vec) {
     std::cout << std::endl;
 }
 
+template <class T> 
+void bubbleSort(std::vector<T> & vec) { 
+    for (unsigned int i = 0; i < vec.size(); i++) { 
+        for (unsigned int j = 0; j < vec.size() - 1; j++) { 
+            if (vec[j] > vec[j + 1]) { 
+                std::swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+}
+
 template <class T>
 void quickSort(std::vector<T> & vec, int low, int high) { 
     if (low >= high) { return; }
-    int pivot = high;
     int start = low;
     for (int i = low; i < high; i++) { 
-        if (vec[i] < vec[pivot]) { 
-            T temp = vec[start];
-            vec[start] = vec[low];
-            vec[low] = temp;
+        if (vec[i] <= vec[high]) { 
+            std::swap(vec[start], vec[i]);
             start++;
         }
     }
-    T temp = vec[pivot];
-    vec[pivot] = vec[start];
-    vec[start] = temp;
-    print(vec);
+    std::swap(vec[start], vec[high]);
     quickSort(vec, low, start - 1);
     quickSort(vec, start + 1, high);
 }
@@ -48,5 +53,7 @@ int main() {
     std::vector<int> nums2 = {3, 7, 1, 8, 4, 11, 20, 5};
     print(nums2);
     quickSort(nums2);
+    print(nums2);
+    bubbleSort(nums2);
     print(nums2);
 }
