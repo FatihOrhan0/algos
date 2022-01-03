bd613d149f7dddbd0ef5f6e53c7ba6ea2957ec4f
Fatih ORHAN <orhanfatih2002@gmail.com>
2022-01-02 21:53:07 -0500

    added the merge function

--- sorting.cpp
+++ sorting.cpp
@@ -71,16 +71,53 @@ void quickSort(std::vector<T> & vec) {
     quickSort(vec, 0, vec.size() - 1);
 }
 
+template <class T> 
+std::vector<T> merge(const std::vector<T> & vec1, std::vector<T> & vec2) { 
+    std::vector<T> combined(vec1.size() + vec2.size());
+    unsigned int i = 0;
+    unsigned int j = 0;
+    unsigned int index = 0;
+    while (i < vec1.size() && j < vec2.size()) { 
+        if (vec1[i] < vec2[j]) { 
+            combined[index] = vec1[i];
+            i++; index++;
+        }
+        else { 
+            combined[index] = vec2[j];
+            j++; index++;
+        }
+    } 
+    for (; i < vec1.size(); i++) { 
+        combined[index] = vec1[i];
+        index++;
+    }
+    for (; j < vec2.size(); j++) { 
+        combined[index] = vec2[j];
+        index++;
+    }
+    return combined;
+}
+
+template <class T> 
+void mergeSort(std::vector<T> & vec) { 
+
+}
+
+template <class T> 
+void heapSort(std::vector<T> & vec) { 
+    
+}
+
 int main() { 
     //quicksort test:
-   /*  std::vector<int> nums(200);
+    std::vector<int> nums(200);
     for (int i = 0; i < 200; i++) { 
         nums[i] = 200 - i;
     }
     print(nums);
     std::cout << "sorted:" << std::endl;
     quickSort<int>(nums);
-    print(nums); */
+    print(nums); 
     std::vector<int> nums2 = {3, 7, 1, 8, 4, 11, 20, 5};
     print(nums2);
     selectionSort(nums2);
