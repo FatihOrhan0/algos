9b0d427ff84793d2d5508b7be9e98fa5a3ab330d
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-02-05 21:23:57 -0500

    rewrote

--- trappingRainWater.cpp
+++ trappingRainWater.cpp
@@ -6,50 +6,48 @@
 //https://leetcode.com/problems/trapping-rain-water/submissions/
 //TODO: reconsider the last segment.
 
+/* 
+    *General structure of the algorithm: 
+    *span through the trailing zeros at the beginning 
+    *for the first nonzero value, keep traversing if the upcoming numbers are greater than the current.
+    *when the series starts decreasing, find a value that is greater thatn or equal to the starting value
+    *add the water in between the two values. 
+    *if no such value is found increase the left index.
+*/
+
 int trap(const std::vector<int> & height) { 
     int total = 0;
     unsigned int leftIndex = 0;
-    while (leftIndex < height.size() - 1) {
-
-        if (height[leftIndex + 1] >= height[leftIndex]) { 
-            leftIndex++;
-            continue;
-        }
-
-        unsigned int localIndex = leftIndex + 1;
-        unsigned int localIndex2 = leftIndex + 1;
-        bool exceed = false;
-        while (height[localIndex] <= height[leftIndex]) { 
-            localIndex++;
-            if (localIndex == height.size()) { 
-                exceed = true;
-                break;
-            }
-        }
-
-        if (exceed) { 
-            std::vector<int> temp(height.size() - leftIndex);
-            for (unsigned int i = 0; i < height.size() - leftIndex; i++) { 
-                temp[i] = height[leftIndex + 1];
-            }
-            std::reverse(temp.begin(), temp.end());
-            total += trap(temp);
-            return total;
-        }
-        
-        unsigned int min = std::min(height[localIndex], height[leftIndex]);
-        while (localIndex2 < localIndex) { 
-            total += min - height[localIndex2];
-            localIndex2++;
-        }
-        leftIndex = localIndex;
+    while (leftIndex < height.size()) { 
+    //skip to the decreasing part
+    while (leftIndex < height.size() - 1 && height[leftIndex] <= height[leftIndex + 1]) { 
         leftIndex++;
     }
-    std::cout << "total: " << total << std::endl;
+    std::cout << "leftIndex: " << leftIndex << std::endl;
+    unsigned int tempIndex = leftIndex;
+    tempIndex++;
+    while (tempIndex < height.size() && height[tempIndex] < height[leftIndex]) { 
+        tempIndex++;
+    }
+    std::cout << "leftIndex: " << leftIndex << " " << tempIndex << std::endl;
+    if (tempIndex == height.size()) { 
+        leftIndex++;
+        continue;
+    }
+    else { 
+        for (unsigned int tempIndex2 = leftIndex; tempIndex2 < tempIndex; tempIndex2++) { 
+            total += height[leftIndex] - height[tempIndex2];
+        }
+    }
+    leftIndex = tempIndex;
+    std::cout << "leftIndex: " << leftIndex << " " << tempIndex << std::endl;
+    }
     return total;
 }
 
 int main() { 
-    std::vector<int> heights = {0,1,0,2,1,0,1,3,2,1,2,1};
-    std::cout << "trap: " << trap(heights);
+    std::vector<int> heights = {0,1,0,5,0,3};
+    std::vector<int> heights2 = {2, 0, 0,0, 8};
+    std::cout << trap(heights) << std::endl;
+    std::cout << trap(heights2);
 }
