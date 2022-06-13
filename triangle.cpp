2e5d9d66cd83c52d5595e13c685e77d90ec5d339
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-06-12 22:03:08 -0400

    Solution to today's LC problem

--- triangle.cpp
+++ triangle.cpp
@@ -26,8 +26,3 @@ int minimumTotal(vector<vector<int> > & triangle) {
     }
     return ans;
 }
-
-int main() { 
-    vector<vector<int> > triangle = [{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}};
-    cout << minimumTotal(triangle) << endl;
-}
