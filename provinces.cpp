ac18e3f00ed9527d773ce2ea564f70ff358a9e39
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-06-27 04:04:27 -0400

    Solution to LC547

---
+++ provinces.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Disjoint { 
+public:
+    
+    vector<int> roots;
+    vector<int> ranks;
+    
+    Disjoint(const int n) { 
+        roots = vector<int>(n, 0);
+        ranks = vector<int>(n);
+        for (int i = 0; i < n; i++) { 
+            ranks[i] = 1;
+            roots[i] = i;
+        }
+    }
+    int find(const int n) { 
+        if (roots[n] == n) return n;
+        return roots[n] = find(roots[roots[n]]);
+    }
+    
+    void unionSet(const int n1, const int n2) { 
+        int r1 = find(n1);
+        int r2 = find(n2);
+        if (roots[r1] != roots[r2]) { 
+            if (ranks[r1] > ranks[r2]) {
+                roots[r2] = r1;
+            }
+            else if (ranks[r2] > ranks[r1]) { 
+                roots[r1] = r2;
+            }
+            else { 
+                roots[r2] = r1;
+                ranks[r1]++;
+            }
+        }
+    }
+    
+    bool isConnected(const int n1, const int n2) { 
+        return find(n1) == find(n2);
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int> > & isConnected) {
+        int n = isConnected.size();
+        Disjoint d(n);
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n; j++) { 
+                if (isConnected[i][j] == 1) {
+                    d.unionSet(i, j);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < d.roots.size(); i++) { 
+            cout << d.roots[i] << endl;
+            if (d.roots[i] == i) count++;
+        }
+        return count;
+    }
+};
+
+int main() { 
+    Solution s;
+    vector<vector<int> > test = {{1,1,0},{1,1,0},{0,0,1}};
+    cout << s.findCircleNum(test);
+
+    return EXIT_SUCCESS;
+}
+
